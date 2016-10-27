0868f7bba2b30c18aedf42db948ce4350f943bb4
Pascal Boutin <pboutin@webcliniq.com>
2016-10-26 23:25:56 -0400

    added the almanax data

---
+++ src/AlmanaxData.py
@@ -0,0 +1,56 @@
+# coding=utf-8
+
+from WebScraper import WebScraper
+
+import re
+import json
+import requests
+import os
+import os.path
+import timeit
+import datetime
+import urllib
+from pyquery import PyQuery as pq
+
+class AlmanaxData(WebScraper):
+    outputFile = 'almanax-data'
+    baseUrl = 'http://www.krosmoz.com/fr/almanax'
+
+
+    def getItemUrls(self):
+        year = '2016' # leap year
+        months = [31,29,31,30,31,30,31,31,30,31,30,31]
+
+        urls = []
+        currentMonth = 1
+        for daysCount in months:
+            month = self.padTwoDigit(str(currentMonth))
+            for day in range(daysCount):
+                day = self.padTwoDigit(str(day + 1))
+                urls.append("%s/%s-%s-%s" % (self.baseUrl, year, month, day))
+            currentMonth = currentMonth + 1
+
+        return urls
+
+
+    def parsePage(self, itemPage, itemUrl):
+        itemId = self.extractFrom(itemUrl, u'\d{2}-\d{2}$')
+
+        rawBonus = itemPage('div.dofus div.more').text()
+
+        rawQuest = itemPage('div.dofus div.more p.fleft').text()
+        rawQuest = self.extractFrom(rawQuest, r'\d.+')
+
+        item = {
+            'bonus': self.extractFrom(rawBonus, r'.+\.'),
+            'quest': re.sub(r' et rapporter.*', '', rawQuest)
+        }
+
+        return (itemId, item)
+
+
+    def padTwoDigit(self, input):
+        return input if len(input) is 2 else '0' + input
+
+
+AlmanaxData()
