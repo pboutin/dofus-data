37f252ff6590e537937fec6cd64206ffebeda9ca
Pascal Boutin <pboutin@webcliniq.com>
2016-10-26 23:04:54 -0400

    Refactored the whole web scraping utility to create a reusable framework

---
+++ src/DofusData.py
@@ -0,0 +1,93 @@
+# coding=utf-8
+
+from WebScraper import WebScraper
+
+import re
+import json
+import requests
+import os
+import os.path
+import timeit
+import datetime
+import urllib
+from pyquery import PyQuery as pq
+
+class DofusData(WebScraper):
+    outputFile = 'dofus-data'
+    baseUrl = 'http://www.dofus.com'
+    imageBaseUrl = 'http://staticns.ankama.com/dofus/www/game/items/200'
+
+
+    def getItemUrls(self):
+        ressourcesIndexes = [
+            '/fr/mmorpg/encyclopedie/armes',
+            '/fr/mmorpg/encyclopedie/equipements',
+            '/fr/mmorpg/encyclopedie/consommables',
+            '/fr/mmorpg/encyclopedie/ressources'
+        ]
+
+        urls = []
+
+        for ressourcesEndpoint in ressourcesIndexes:
+            currentPage = 1
+
+            while True:
+                indexUrl = "%s%s?page=%s"  % (self.baseUrl, ressourcesEndpoint, str(currentPage))
+                self.printInfo("Fetching : %s" % indexUrl)
+                listPage = self.loadPageFrom(indexUrl)
+
+                itemRows = listPage('table.ak-table tbody tr')
+
+                if not itemRows:
+                    break
+
+                for itemRow in itemRows:
+                    endpoint = pq(itemRow).find('td').eq(1).find('a').attr('href')
+                    urls.append(self.baseUrl + endpoint)
+
+                currentPage = currentPage + 1
+
+            self.printSuccess("Url processing for '%s' : done, Urls count : %s" % (ressourcesEndpoint, str(len(urls))))
+        return urls
+
+
+    def parsePage(self, itemPage, itemUrl):
+        itemId = self.extractFrom(itemUrl, u'\/([\w\-ō]+)$')
+
+        recipe = self._parseRecipe(itemPage('div.ak-crafts div.ak-list-element'))
+
+        rawImageSrc = itemPage('div.ak-encyclo-detail-illu img').attr('src')
+        imageUrl = self.imageBaseUrl + self.extractFrom(rawImageSrc, u'\/\d+.png$')
+
+        try:
+            urllib.urlretrieve(imageUrl, '../images/' + itemId + '.png')
+        except:
+            self.isFailing = True
+            self.printWarning("Failed to download image : %s" % imageUrl)
+
+        item = {
+            'name': self.extractFrom(itemPage('div.ak-title-container h1').text()),
+            'level': self.extractFrom(itemPage('div.ak-encyclo-detail-level').text(), u'\d+'),
+            'type': self.extractFrom(itemPage('div.ak-encyclo-detail-type span').text()),
+            'link': itemUrl.replace(self.baseUrl, ''),
+            'recipe': recipe
+        }
+
+        return (itemId, item)
+
+
+    def _parseRecipe(self, rawRecipeElements):
+        recipe = None
+        if rawRecipeElements:
+            recipe = {}
+            for recipeElement in rawRecipeElements:
+                rawRecItemId = pq(recipeElement).find('div.ak-content a').attr('href')
+                rawRecItemQuantity = pq(recipeElement).find('div.ak-front').text()
+
+                recItemId = self.extractFrom(rawRecItemId, u'\/([\w\-ō]+)$')
+                recItemQuantity = self.extractFrom(rawRecItemQuantity, r'\d+')
+                recipe[recItemId] = recItemQuantity
+
+        return recipe
+
+DofusData()
---
+++ src/WebScraper.py
@@ -0,0 +1,156 @@
+import re
+import json
+import requests
+import os
+import os.path
+import timeit
+import datetime
+import urllib
+from pyquery import PyQuery as pq
+
+class WebScraper:
+    errors = []
+    baseUrl = ''
+    outputFile = 'default'
+
+    isFailing = False
+
+    def __init__(self):
+        startTime = timeit.default_timer()
+        errorFilePath = "../%s.errors.json" % self.outputFile
+        outputFilePath = "../%s.json" % self.outputFile
+
+        if os.path.isfile(outputFilePath):
+            with open(outputFilePath) as dataFile:
+                self.printSuccess("Updating the current '%s.json'" % self.outputFile)
+                processedData = json.load(dataFile)
+        else:
+            self.printSuccess('Processing from scratch')
+            processedData = {}
+
+        if os.path.isfile(errorFilePath):
+            self.printSuccess('Recovering from pending errors')
+            with open(errorFilePath) as errorFile:
+                pastErrors = json.load(errorFile)
+                urls = [pastError['url'] for pastError in pastErrors]
+        else:
+            self.printSuccess('Loading URLs...')
+            urls = self.getItemUrls()
+
+        self.printSuccess('Initialization : done')
+
+        for url in urls:
+            self.printInfo("Loading : %s" % url)
+            page = self.loadPageFrom(url)
+            if page is not None:
+                keyValue = self._parsePage(page, url)
+                if keyValue is not None:
+                    processedData[keyValue[0]] = keyValue[1]
+
+        self.printSuccess('Items processing : done')
+
+        self.printSuccess('Urls: %s' % str(len(urls)))
+        self.printSuccess('Keys: %s' % str(len(processedData.keys())))
+
+        with open(outputFilePath, 'w') as dataOutput:
+            json.dump(processedData, dataOutput)
+            self.printSuccess('Data writing : done')
+
+        errorCount = len(self.errors)
+        if errorCount:
+            self.printError("%s errors reported, writing the details..." % errorCount)
+            with open(errorFilePath, 'w') as errorOutput:
+                json.dump(self.errors, errorOutput)
+                self.printSuccess('Error logs writing : done')
+        else:
+            self.printSuccess('Full success, cleaning error logs...')
+            if os.path.isfile(errorFilePath):
+                os.remove(errorFilePath)
+
+        endTime = timeit.default_timer()
+        runtime = (endTime - startTime) / 60
+        self.printSuccess('Finish (' + str(runtime) + ' minutes)')
+
+
+    def getItemEndpoints(self):
+        raise Exception('You must implement "getItemEndpoints()" and return and array of URLs.')
+
+
+    def _parsePage(self, page, url, attempt=0):
+        self.isFailing = False
+
+        if attempt > 0:
+            self.printWarning('Parsing attempt - ' + str(attempt) + ' - ' + url)
+
+        keyValue = self.parsePage(page, url)
+
+        if self.isFailing:
+            if attempt < 5:
+                return self._parsePage(page, url, attempt + 1)
+            self.printAndLogError('Failed to parse', url)
+            return None
+
+        return keyValue
+
+
+    def parsePage(self, page, url):
+        raise Exception('You must implement "parsePage(page, url)" and return a tuple as (key, value).')
+
+
+    def extractFrom(self, string, pattern=None):
+        if not self.isString(string):
+            self.isFailing = True
+            return ''
+
+        if pattern is None:
+            return string
+
+        matches = re.findall(pattern, string)
+        return matches[0] if len(matches) else ''
+
+
+    def loadPageFrom(self, url, attempt=0):
+        if attempt > 0:
+            self.printWarning('Loading attempt - ' + str(attempt) + ' - ' + url)
+
+        try:
+            response = requests.get(url, verify=True, timeout=5)
+
+            if response.status_code == 404:
+                self.printAndLogError('Broken link (404)', url)
+                return None
+
+            return pq(response.text)
+        except:
+            if attempt < 5:
+                return self.loadPageFrom(url, attempt + 1)
+            self.printAndLogError('Failed to load', url)
+            return None
+
+
+    def isString(self, variable):
+        return isinstance(variable, basestring)
+
+
+    def printSuccess(self, message):
+        print '\033[92m' + message + '\033[0m'
+
+
+    def printWarning(self, message):
+        print '\033[93m' + message + '\033[0m'
+
+
+    def printAndLogError(self, message, url):
+        self.errors.append({
+            'message': message,
+            'url': url
+        })
+        self.printError(message)
+
+
+    def printError(self, message):
+        print '\033[91m' + message + '\033[0m'
+
+
+    def printInfo(self, message):
+        print message
